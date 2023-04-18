34c1efdea82323c57ee0cc6a0c9dc37feb5f4acb
Veliaraft <43182375+Veliaraft@users.noreply.github.com>
2023-04-18 16:43:05 +0800

    Add files via upload

--- scripts/codeformatter.js
+++ scripts/codeformatter.js
@@ -4,6 +4,7 @@ let pinks = ['if ', 'elif ', 'else:', 'for ', 'while ', 'pass']
 let aqua = ['_ready()', '_process()', '_process(delta)', 
             '_physics_process(delta)', '_physics_process()', 
             '_init()', '_get(property)']
+let greens = ['Input', 'Vector2', 'Vector3']
 let deeppinks = ['pass']
 for (var i = 0; i < codes.length; i++) {
     reds.forEach(element => {
