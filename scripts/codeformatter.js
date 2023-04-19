b97045de51aef4346c5eb72deec99f25b3f9d7cb
Veliaraft <43182375+Veliaraft@users.noreply.github.com>
2023-04-20 07:57:47 +0800

    Add files via upload

--- scripts/codeformatter.js
+++ scripts/codeformatter.js
@@ -1,11 +1,10 @@
 let codes = document.querySelectorAll('.code');
-let reds = ['extends ', 'func ', 'var ', 'print']
-let pinks = ['if ', 'elif ', 'else:', 'for ', 'while ', 'pass']
+let reds = ['extends ', 'func ', 'var ', 'print', 'const ', ' and ', ' or ', ' load', ' preload', 'onready', '@onready']
+let pinks = ['elif ', 'if ', 'else:', 'for ', 'while ', 'pass', 'return']
 let aqua = ['_ready()', '_process()', '_process(delta)', 
             '_physics_process(delta)', '_physics_process()', 
-            '_init()', '_get(property)']
+            '_init()', '_input(event)', '_get(property)', 'sum', 'size()', 'instance()', 'get_tree()']
 let greens = ['Input', 'Vector2', 'Vector3']
-let deeppinks = ['pass']
 for (var i = 0; i < codes.length; i++) {
     reds.forEach(element => {
         codes[i].innerHTML = codes[i].innerHTML.replaceAll(element, '<text style="color: #ff4040;">'+element+'</text>')
@@ -13,10 +12,10 @@ for (var i = 0; i < codes.length; i++) {
     aqua.forEach(element => {
         codes[i].innerHTML = codes[i].innerHTML.replaceAll(element, '<text style="color: aqua;">'+element+'</text>')
     });
-    deeppinks.forEach(element => {
+    pinks.forEach(element => {
         codes[i].innerHTML = codes[i].innerHTML.replaceAll(element, '<text style="color: deeppink;">'+element+'</text>')
     });
-    exceptions.forEach(element => {
-        codes[i].innerHTML = codes[i].innerHTML.replaceAll(element, '<text style="color: grey;">'+codes[i].innerHTML.match(element)+'</text>')
+    greens.forEach(element => {
+        codes[i].innerHTML = codes[i].innerHTML.replaceAll(element, '<text style="color: MediumSpringGreen;">'+element+'</text>')
     });
 }
