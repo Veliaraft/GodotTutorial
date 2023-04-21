b0aeab56f4f3873ff5a6eb59834f06e1a7f6ac00
Veliaraft <43182375+Veliaraft@users.noreply.github.com>
2023-04-21 16:44:33 +0800

    Add files via upload

--- scripts/codeformatter.js
+++ scripts/codeformatter.js
@@ -1,11 +1,24 @@
-built_in = 'Color8 ColorN abs acos asin atan atan2 bytes2var cartesian2polar ceil char clamp convert cos cosh db2linear decimals dectime deg2rad dict2inst ease exp floor fmod fposmod funcref get_stack hash inst2dict instance_from_id inverse_lerp is_equal_approx is_inf is_instance_valid is_nan is_zero_approx len lerp lerp_angle linear2db load log max min move_toward nearest_po2 ord parse_json polar2cartesian posmod pow preload print_stack push_error push_warning rad2deg rand_range rand_seed randf randi randomize range_lerp round seed sign sin sinh smoothstep sqrt step_decimals stepify str str2var tan tanh to_json type_exists typeof validate_json var2bytes var2str weakref wrapf wrapi bool int float String NodePath Vector2 Rect2 Transform2D Vector3 Rect3 Plane Quat Basis Transform Color RID Object NodePath Dictionary Array PoolByteArray PoolIntArray PoolRealArray PoolStringArray PoolVector2Array PoolVector3Array PoolColorArray';
-literal = 'true false null';
-
 var codes = document.querySelectorAll('.code');
 
 for (var i = 0; i < codes.length; i++) {
-    codes[i].innerHTML = codes[i].innerHTML.replace(/(and|in|not|or|self|void|as|assert|breakpoint|class|class_name|extends|is|func|setget|signal|tool|yield|const|enum|export|onready|static|var|break|continue|if|elif|else|for|pass|return|match|while|remote|sync|master|puppet|remotesync|mastersync|puppetsync|true|false|null)([^a-z0-9\$_])/gi, '<text style="color: #ff4040;">$1 </text>');
-    codes[i].innerHTML = codes[i].innerHTML.replace(/(\b\d+)/gi, '<text style="color: MediumSpringGreen;">$1</text>')
+    target = codes[i].innerHTML.split('\n')
+    codes[i].innerHTML = '';
+    var a = '';
+    for (var j = 0; j < target.length; j++){
+        //a = target[i].match(/^(#.*)/g);
+        //target[i].replace(/^(#.*)/g, '');
+        target[j] = target[j].replaceAll(/(\".+\")|(\'.+\')/g, '<text style="color: SandyBrown;">$1</text>')
+        target[j] = target[j].replaceAll(/(func |extends |const |var |enum | and | or |onready |@onready |true|false|null|static)/g, '<text style="color: #ff4040;">$1</text>')
+        target[j] = target[j].replaceAll(/([A-Za-z_0-9]+\(.*\))/g, '<text style="color: aqua;">$1</text>')
+        target[j] = target[j].replaceAll(/(if |elif |else:|for |while |pass|return)/g, '<text style="color: deeppink;">$1</text>')
+        target[j] = target[j].replaceAll(/(HSlider|VSlider|Input|Vector2|Vector3|\$|GDScript|Reference|AudioStreamPlayer|AudioStreamMP3|AudioStreamOGGVorbis|Sprite|Node|Node2D|KinematicBody|StaticBody|RigidBody)/g, '<text style="color: MediumSpringGreen;">$1</text>')
+        target[j] = target[j].replaceAll(/(\b\d+)/g, '<text style="color: MediumSpringGreen;">$1</text>');
+        //target[i] += a;
+        //a = '';
+        //console.log(target[j])
+        codes[i].innerHTML += target[j] + '\n';
+    }
+    
     // aqua.forEach(element => {
     //     codes[i].innerHTML = codes[i].innerHTML.replaceAll(element, '<text style="color: aqua;">'+element+'</text>')
     // });
